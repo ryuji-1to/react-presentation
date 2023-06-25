47b3d22e48556dc8f9b77231aa9b59703df689f1
ryuji-1to <ryuji0222ss@icloud.com>
2023-06-25 17:46:25 +0900

    ✨ add font size

--- src/components/template/parts/Text.tsx
+++ src/components/template/parts/Text.tsx
@@ -1,10 +1,28 @@
+type Size = 'sm' | 'md' | 'lg' | 'xl';
+
 type Props = {
   children: React.ReactNode;
-  size?: 'sm' | 'md' | 'lg' | 'xl';
+  size?: Size;
 } & React.ComponentPropsWithoutRef<'p'>;
+
+function getFontSize(size: Size) {
+  switch (size) {
+    case 'sm':
+      return 'text-md';
+    case 'lg':
+      return 'text-xl';
+    case 'xl':
+      return 'text-2xl';
+    case 'md':
+    default:
+      return 'text-lg';
+  }
+}
+
 export function Text({ children, size = 'md', className, ...rest }: Props) {
+  const fontSize = getFontSize(size);
   return (
-    <p {...rest} className={className}>
+    <p {...rest} className={`${className} ${fontSize}`}>
       {children}
     </p>
   );
