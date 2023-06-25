620a421d8ab79978ee6ad401c29053aca5dc549f
ryuji-1to <ryuji0222ss@icloud.com>
2023-06-25 17:45:25 +0900

    🔥 remove unused

--- src/hooks/util/use-clickOutside.ts
+++ src/hooks/util/use-clickOutside.ts
@@ -1,13 +1,10 @@
 import { useEffect } from 'react';
 
 export function useClickOutside<T extends HTMLElement>(ref: React.RefObject<T>, callback: () => void) {
-  console.log(ref.current);
-
   const handleClick = (e: MouseEvent) => {
     if (!ref.current || ref.current.contains(e.target as Node)) {
       return;
     }
-    console.log(e.target, ref.current);
     callback();
   };
   useEffect(() => {
