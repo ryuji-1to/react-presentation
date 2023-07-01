e231476f3ee48809805920468474233433e833cf
ryuji-1to <ryuji0222ss@icloud.com>
2023-07-01 09:31:54 +0900

    ✨ add mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
+import remarkGfm from 'remark-gfm';
+
+const options = {
+  remarkPlugins: [remarkGfm],
+  rehypePlugins: []
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES ? 'react-presentation' : './',
-  plugins: [react(), mdx()]
+  plugins: [react(), { enforce: 'pre', ...mdx(options) }]
 });
